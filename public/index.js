023ee392e31ec1ff554a19bf2c86c08bd3b515a5
paulespinnozarl <paulespinozarl@gmail.com>
2024-03-05 12:55:14 -0300

    cv name

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-export const cvpaul = "cv-02-24.pdf";
+export const cvpaul = "cv-paul-espinoza.pdf";
 export const rollet = "rolleet.jpg";
 export const logo_pico = "logo-nawara-pico.png";
 export const iupsm2 = "santiago-mariño.jpg";
